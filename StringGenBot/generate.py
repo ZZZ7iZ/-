b987389cce32b63dbc34351cb8853acbe2f8ad0e
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-06-12 23:55:23 +0300

    Update generate.py

--- StringGenBot/generate.py
+++ StringGenBot/generate.py
@@ -38,7 +38,9 @@ ask_ques = "**اضغط لبدا استخراج جلسة**"
 buttons_ques = [
     [
         InlineKeyboardButton("‹ بايروجرام ›", callback_data="pyrogram1"),
-   
+   ],
+
+]
    
         InlineKeyboardButton("‹ تيرمكس ›", callback_data="telethon")
     ],
